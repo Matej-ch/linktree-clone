caa75344e074cca9451708a3626f148719ba476e
Matej <matejchalachan@gmail.com>
2022-03-07 20:01:56 +0100

    added tailwind library

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        './templates/**/*.html.twig',
+        './assets/**/*.{js,jsx,ts,tsx,vue}',
+        './assets/**/*.scss'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: 'class',
+        }),
+        require('@tailwindcss/typography'),
+    ],
+}
