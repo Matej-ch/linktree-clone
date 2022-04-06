62bd79c10129fcfe27d267f58090d3e03cd2a10f
Matej <matejchalachan@gmail.com>
2022-04-06 21:16:22 +0200

    added transition function and transition duration

---
+++ migrations/Version20220406181627.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220406181627 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user ADD transition_function VARCHAR(128) DEFAULT NULL, ADD transition_duration DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6495E237E06 ON user (name)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX UNIQ_8D93D6495E237E06 ON user');
+        $this->addSql('ALTER TABLE user DROP transition_function, DROP transition_duration');
+    }
+}
