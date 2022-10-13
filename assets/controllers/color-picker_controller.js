a80fcbe546248ec72e412c1924894710d0729692
Matej <matejchalachan@gmail.com>
2022-10-13 20:58:41 +0200

    updated packages

--- assets/controllers/color-picker_controller.js
+++ assets/controllers/color-picker_controller.js
@@ -1,4 +1,4 @@
-import {Controller} from "@hotwired/stimulus"
+import {Controller} from '@hotwired/stimulus';
 import '@easylogic/colorpicker/dist/colorpicker.css';
 import ColorPickerUI from '@easylogic/colorpicker'
 
